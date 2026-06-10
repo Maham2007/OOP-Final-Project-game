3a5d5bf93475fa027b67c25a96aeeaf7271c7e7d
Maham2007 <mahhamm2007@gmail.com>
2026-06-10 23:21:54 +0500

    Update Piece.h

--- Piece.h
+++ Piece.h
@@ -1,25 +1,66 @@
-#ifndef PIECE_H
-#define PIECE_H
-
-#include <cmath>
+#pragma once
+#include <vector>
+#include <string>
 using namespace std;
-
-class Board; // forward declaration
-
 class Piece {
 protected:
+    char color; // 'W' or 'B'
     char symbol;
 
 public:
-    Piece(char s) : symbol(s) {}
-
+    Piece(char c, char s);
     virtual ~Piece() {}
 
-    char getSymbol() {
-        return symbol;
-    }
+    char getColor() const;
+    char getSymbol() const;
 
-    virtual bool isValidMove(int sx, int sy, int dx, int dy, Board* board) = 0;
+    virtual bool isValidMove(int fromRow, int fromCol, int toRow, int toCol, Piece* board[8][8]) = 0;
+
+    bool isEnemy(Piece* other) const;
+    bool isPathClear(int fromRow, int fromCol, int toRow, int toCol, Piece* board[8][8]) const;
+};
+// ---- Derived Piece Classes ----
+
+class Pawn : public Piece {
+public:
+    bool hasMoved;             // tracks first move (for 2-square advance)
+    bool enPassantVulnerable;  // true for exactly one turn after a 2-square advance
+
+    Pawn(char c);
+    bool isValidMove(int fromRow, int fromCol, int toRow, int toCol, Piece* board[8][8]) override;
 };
 
-#endif
+class Rook : public Piece {
+public:
+    bool hasMoved;  // needed for castling legality
+
+    Rook(char c);
+    bool isValidMove(int fromRow, int fromCol, int toRow, int toCol, Piece* board[8][8]) override;
+};
+
+class Knight : public Piece {
+public:
+    Knight(char c);
+    bool isValidMove(int fromRow, int fromCol, int toRow, int toCol, Piece* board[8][8]) override;
+};
+
+class Bishop : public Piece {
+public:
+    Bishop(char c);
+    bool isValidMove(int fromRow, int fromCol, int toRow, int toCol, Piece* board[8][8]) override;
+};
+
+class Queen : public Piece {
+public:
+    Queen(char c);
+    bool isValidMove(int fromRow, int fromCol, int toRow, int toCol, Piece* board[8][8]) override;
+};
+
+class King : public Piece {
+public:
+    bool hasMoved;  // needed for castling legality
+
+    King(char c);
+    // Normal 1-square moves only; castling is handled in Board::movePiece
+    bool isValidMove(int fromRow, int fromCol, int toRow, int toCol, Piece* board[8][8]) override;
+};
